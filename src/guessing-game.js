3c6f2c4c317341590ffe4977e6c22609874844ca
Aguloff <agulovu@gmail.com>
2023-09-23 21:20:06 +0300

    feat: add complited task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+       this.current = Math.round((this.max + this.min) / 2);
+       return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
